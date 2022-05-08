52e55b4bbbada2c39d5a6adea38948524387e390
Kaiyrgali Bekkali <kairgalib@ya.ru>
2022-05-08 13:24:32 +0500

    move Menu

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -1,23 +1,28 @@
-import React, { useState, useRef, useMemo } from 'react';
+import React, { useState, useRef, useMemo, useEffect } from 'react';
 import './Main.css';
-import { store } from '../../service/getJson';
-import { getElements} from '../../units/getElements';
+import { getElements } from '../../units/getElements';
 import MenuItem from '../MenuItem';
 import Details from '../Details';
 
+const  _apiBase = 'https://gist.githubusercontent.com/avydashenko/e1702c1ef26cddd006da989aa47d4f62/raw/633c9211d1a1816d8286368d9ff9717f343c508a/view.json';
+
 function Main() {
   const [refresh, setRefresh] = useState(false);
-  const [renderCount, setRenderCount] = useState(1);
   const [active, setActive] = useState(null);
+  const [list, setList] = useState(null);
 
-  const elem = useMemo(()=> {
-    return getElements(store)
+  useEffect(() => {
+    console.log('use effect run');
+    fetch(_apiBase)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log('fetch data >', data);
+        const elem = getElements(data)
+        setList(elem)
+      })
   }, [refresh]);
-  const [currentElem, setElem] = useState(elem);
-
-  console.log('elem >', elem);
-  console.log('currentElem >', currentElem)
-  console.log('active >', active);
 
   const changeActiveLabel =(item) => {
     setActive(item);
@@ -25,26 +30,25 @@ function Main() {
 
   const removeElement = () => {
     if (!active) {return}
-    const indexActive = currentElem.indexOf(active);
-    const nextElem = currentElem.slice(indexActive+1);
-    const isLast = indexActive === currentElem.length-1;
+    const indexActive = list.indexOf(active);
+    const nextElem = list.slice(indexActive+1);
+    const isLast = indexActive === list.length-1;
     const next = nextElem.findIndex(el => el.level+1 === active.level+1) + 1;
     const indexNext = active.level === 3 || isLast
       ? 1 
       : next === 0 ? 100 : next;
-    // // forEach((element, indexOf(active.level, indexActive);
-    // console.log('current before delete>', currentElem);
-    // console.log('Try remove >', indexActive, active.level, next);
-    currentElem.splice(indexActive,indexNext)
-    setElem( prev => prev.slice());
+    list.splice(indexActive,indexNext)
+    setList( prev => prev.slice());
     setActive(null);
   }
+
+  if (list === null) {return <div className="Main">Loading...</div>}
 
   return (
     <div className="Main">
       <div className="Container">
         <div className="Menu">
-          {currentElem.map((item, i)=> 
+          {list.map((item, i)=> 
             <MenuItem
               key={i}
               label={item}
@@ -60,12 +64,8 @@ function Main() {
         <button onClick={()=>removeElement()}>
           Remove
         </button>
-        <button onClick={()=>setRenderCount(prev=>prev+1)}>
-          Render {renderCount}
-        </button>
         <button onClick={()=>{
           setRefresh(prev=>!prev);
-          setElem(elem);
         }}>
           Refresh {refresh}
         </button>
--- src/service/getData.js
+++ src/service/getData.js
@@ -1,17 +1,8 @@
 const  _apiBase = 'https://gist.githubusercontent.com/avydashenko/e1702c1ef26cddd006da989aa47d4f62/raw/633c9211d1a1816d8286368d9ff9717f343c508a/view.json';
-// const   _apiBaseKey = 'qwerty123';
 
 const  getData = async () => {
   const res = await fetch(_apiBase, {
           method: "GET",
-          // mode: 'no-cors',
-          // headers: {
-          // //   'Content-Type': 'application/json',
-          // //   'Authentication': _apiBaseKey,
-          //   'Access-Control-Allow-Origin': '*',
-          //   // 'mode': 'cors',
-          // },
-          // body: JSON.stringify()
         });
     if (!res.ok) {
       throw new Error(`Could not fetch Calls, DataBase received ${res.status}`);
